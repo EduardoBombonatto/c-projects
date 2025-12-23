daedd036cf67e07aafc7d07de5136ef632f354ff
EduardoBombonatto <eduardobombonattolorenzetti@gmail.com>
2025-12-23 14:37:24 -0300

    data_structures: array pt2

--- data_structures/arrays/array_challenges.c
+++ data_structures/arrays/array_challenges.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int* reverseArray(int* arr, int n)
 {
@@ -16,6 +17,7 @@ int* reverseArray(int* arr, int n)
     return arr;
 }
 
+//rotate the array "d" times to the left
 int* leftRotate(int* arr, int d, int n)
 {
     printf("Array Size: %d\n", n);
@@ -24,21 +26,52 @@ int* leftRotate(int* arr, int d, int n)
     int* rotatedArr = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
     {
-        rotatedArr[i] = arr[(i + d) % n];
+        rotatedArr[i] = arr[(i - d) % n];
     }
     return rotatedArr;
 }
 
+int* matchingStrings(char** stringList, int stringListCount, char** queries, int queriesCount, int* result_count)
+{
+    int* results = (int*)malloc(queriesCount * sizeof(int));
+    for (int i = 0; i < queriesCount; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < stringListCount; j++)
+        {
+            if (strcmp(queries[i], stringList[j]) == 0)
+            {
+                count++;
+            }
+        }
+        results[i] = count;
+    }
+    *result_count = queriesCount;
+    return results;
+}
+
 int main()
 {
-    int arr1[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr1) / sizeof(arr1[0]);
-    int* reversedArr = leftRotate(arr1, 2, n);
-    printf("Rotate Left twice: ");
-    for (int i = 0; i < n; i++)
-    {   
-        printf("%d ", reversedArr[i]);
+    int stringListCount = 3;
+    int queriesCount = 3;
+    char** stringList = (char**) malloc(stringListCount * sizeof(char*));\
+    stringList[0] = "def";
+    stringList[1] = "de";
+    stringList[2] = "fgh";
+    char** queries = (char**) malloc(queriesCount * sizeof(char*));
+    queries[0] = "de";
+    queries[1] = "lmn";
+    queries[2] = "fgh";
+    int res_count;
+    int* results = matchingStrings(stringList, stringListCount, queries, queriesCount, &res_count);
+
+    for (int i = 0; i < res_count; i++)
+    {
+        printf("%d ", results[i]);
     }
     printf("\n");
+    free(results);
+    free(stringList);
+    free(queries);
     return 0;
 }
