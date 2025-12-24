e7ac3920facddca432e71d2269ee2674bc11bf8a
EduardoBombonatto <eduardobombonattolorenzetti@gmail.com>
2025-12-24 14:23:38 -0300

    linkedList pt2

--- data_structures/linked_list/linkedList_challenge.c
+++ data_structures/linked_list/linkedList_challenge.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
+typedef struct Node
 {
     int val;
-    struct node *next;
+    struct Node *next;
 
-} node;
+} Node;
 
-node *new_node(int val)
+Node *createNode(int val)
 {
-    node *new = (node *)malloc(sizeof(node));
+    Node *new = (Node *)malloc(sizeof(Node));
     if (new == NULL)
     {
         return NULL;
@@ -20,6 +20,45 @@ node *new_node(int val)
     return new;
 }
 
+// insert at the head of the list
+Node *insertHead(Node *head, int x)
+{
+    Node *new = createNode(x);
+    new->next = head;
+    return new;
+}
+
+// free the memory of the list
+void freeMemory(Node *head)
+{
+    Node *aux;
+    while (head != NULL)
+    {
+        aux = head;
+        head = head->next;
+        free(aux);
+    }
+}
+
+// print the linked list
+void printList(Node *list)
+{
+    while (list != NULL)
+    {
+        printf("%d -> ", list->val);
+        list = list->next;
+    }
+    printf("NULL\n");
+}
+
 int main()
 {
+    Node *head = NULL;
+    head = insertHead(head, 4);
+    head = insertHead(head, 3);
+    head = insertHead(head, 2);
+    head = insertHead(head, 1);
+    printList(head);
+    freeMemory(head);
+    return 0;
 }
